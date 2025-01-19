ab9fb03b98e8df4aad47081124ce469eac7a8b75
Verak Luon <luonverakcambo@gmail.com>
2025-01-19 16:31:28 +0700

    home menu

---
+++ app/src/main/java/com/java/android/MainActivity.java
@@ -0,0 +1,23 @@
+package com.java.android;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_food);
+
+
+    }
+}
